a929223e2196fd6a30bf36e6c45b4bccb82dd65d
tibbsm <tibbs.marc@gmail.com>
2018-02-20 13:57:16 +0900

    Combined redundant lines

--- TSP_IO/TSP_IO.py
+++ TSP_IO/TSP_IO.py
@@ -1,32 +1,27 @@
 # Class: 	CS325 - Winter 2018
 # Author: 	Group 45 (Marc Tibbs, Brad Besserman, Michael Chan)
 # Project: 	Traveling Sales Man
-# Program:	Accepts input from testfiles and outputs copied file with .tour extension 
+# Program:	Accepts input from testfiles. 
 
 import sys
 
 def main():
 
 	try:
-		fileName = sys.argv[1]
+		inFile = open(sys.argv[1], 'r')
+		outFile = open(sys.argv[1] + '.tour', 'w')
 	except IndexError:
 		print("[ERROR] Missing argument for testfile. \n")
 		quit()
-
-	try:
-		inFile = open(fileName, 'r')
 	except IOError, OSError:
 		print("[ERROR] Testfile not found. \n")
 		quit()
-
-	outFile = open(fileName + '.tour', 'w')
 
 	cities = []
 	for line in inFile:
 		spLine = line.split()
 		city = {'id':int(spLine[0]), 'x':int(spLine[1]), 'y':int(spLine[2])}
 		cities.append(city)
-
 
 	for city in cities:
 		outFile.write(str(city['id']) + ' ' + str(city['x']) + ' ' + str(city['y']) + '\n')
