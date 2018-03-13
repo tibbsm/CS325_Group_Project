1977759734f134452ae3397da843cc89df52c909
tibbsm <tibbs.marc@gmail.com>
2018-03-13 19:36:19 +0900

    Updating greedy alg. cpp

--- Greedy/CPP/greedy.cpp
+++ Greedy/CPP/greedy.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <vector>
 #include <math.h>
+#include <string>
 
 struct city {
     int id, x, y;
@@ -17,6 +18,7 @@ int tourDistance(std::vector<city> tour){
 
 	for(int i = 1; i < tour.size(); i++){
 		tourDistance += distance(tour.at(i-1),tour.at(i));
+//		std::cout << tourDistance << std::endl;
 	}
 	
 	tourDistance += distance(tour.front(), tour.back());
@@ -41,8 +43,9 @@ double nearest_neighbor(std::vector<city> clist, city curr){
 }
 
 std::vector<city> TSP_NN(std::vector<city> cities){
-    std::vector<city> tour;
-    int best_distance = INT_MAX;
+    std::vector<city> tour = cities;
+    // int best_distance = INT_MAX;
+    int best_distance = tourDistance(cities);
 
     clock_t timeStart = clock();
 
@@ -83,52 +86,63 @@ std::vector<city> TSP_NN(std::vector<city> cities){
 }
 
 void print (std::vector<city> citylist){
-    std::cout << tourDistance(citylist) << std::endl;
-    std::cout << std::endl;
+    // std::cout << tourDistance(citylist) << std::endl;
+    // std::cout << std::endl;
 
     for(std::vector<city>::iterator it = citylist.begin(); it != citylist.end(); ++it) {
         city neigh = *it;
         
         std::cout << neigh.id << " " << neigh.x << " " << neigh.y << std ::endl;
     }
-
 }
 
 int main(int argc, const char * argv[]) {
     
-    std::ifstream infile("tsp_example_3.txt");
-//    std::ifstream infile(argv[1]);
+    // std::ifstream infile("tsp_example_2.txt");
+    std::ifstream infile(argv[1]);
+    std::ofstream outfile(std::string(argv[1])+".tour");
     std::vector<city> cities;
 
     if(infile.is_open()){
         //Begin loop to go through all rows of text file
         while(infile.good()){  
-            
+                
             //city struct
             city init;
             int a;
             // Put city ID, start & finish time into new struct
             while (infile >> a) {
-		        init.id = a;
+    	        init.id = a;
                 infile >> init.x;
                 infile >> init.y;
-                std::cout << init.id  << ", " << init.x  << ", " << init.y  << std::endl;
+                // std::cout << init.id  << ", " << init.x  << ", " << init.y  << std::endl;
                 cities.push_back(init);
 		    }
-            
-            
         }
     }
     else {
         std::cout << "File could not be opened." << std::endl;
     }
+    infile.close();
 
     float total = 0;
 
     clock_t t1, t2;
     t1 = clock();
+	
+	// std::cout << tourDistance(cities) << std::endl;
+    // print(TSP_NN(cities));
+	// std::cout << tourDistance(cities) << std::endl;
 
-    print(TSP_NN(cities));
+    cities = TSP_NN(cities);
+    int best_distance = tourDistance(cities);
+
+    outfile << best_distance << std::endl;
+
+    for(std::vector<city>::iterator it = cities.begin(); it != cities.end(); ++it) {
+        city neigh = *it;
+        outfile << neigh.id << " " << neigh.x << " " << neigh.y << std::endl;
+    }
     
     t2 = clock();
     float diff ((float)t2 - (float)t1);     //Total time
@@ -136,6 +150,8 @@ int main(int argc, const char * argv[]) {
     total = total + seconds;
     std::cout << seconds << " seconds." << std::endl;
 
+
+    // outfile.close();
 
 
     return 0;
